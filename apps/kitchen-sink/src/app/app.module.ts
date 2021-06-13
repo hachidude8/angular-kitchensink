c9a6650c5d7af33703638554958ab88213d85fa5
hachidude8 <alex.dobrean@gmail.com>
2021-06-13 12:32:23 +0200

    Added angular material

--- apps/kitchen-sink/src/app/app.module.ts
+++ apps/kitchen-sink/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
