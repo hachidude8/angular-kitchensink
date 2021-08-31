7027b68574a8aa5eb0bedb763f5da0ba3478a2da
hachidude8 <alex.dobrean@gmail.com>
2021-08-31 13:59:26 +0200

    Renamed MAT_CONFIRMATION_SERVICE_PROVIDER to ConfirmationServiceProvider

--- libs/mat/confirmation/src/mat-confirmation.module.ts
+++ libs/mat/confirmation/src/mat-confirmation.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
-import { MAT_CONFIRMATION_SERVICE_PROVIDER } from './providers';
+import { ConfirmationServiceProvider } from './providers';
 
 @NgModule({
   imports: [CommonModule, ConfirmationModule, MatButtonModule, MatDialogModule],
   declarations: [ConfirmationDialogComponent],
   exports: [ConfirmationModule],
-  providers: [MAT_CONFIRMATION_SERVICE_PROVIDER],
+  providers: [ConfirmationServiceProvider],
 })
 export class MatConfirmationModule {}
--- libs/mat/confirmation/src/providers.ts
+++ libs/mat/confirmation/src/providers.ts
@@ -3,14 +3,8 @@ import { Optional, Provider, SkipSelf } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatConfirmationService } from './mat-confirmation.service';
 
-export const MAT_CONFIRMATION_SERVICE_PROVIDER: Provider = {
+export const ConfirmationServiceProvider: Provider = {
   provide: ConfirmationService,
   deps: [[new Optional(), new SkipSelf(), ConfirmationService], MatDialog],
-  useFactory: (service: ConfirmationService, dialog: MatDialog) => {
-    console.info(
-      `Configuring material confirmation service!`,
-      service ? service : 'Creating default'
-    );
-    return service || new MatConfirmationService(dialog);
-  },
+  useFactory: (service: ConfirmationService, dialog: MatDialog) => service || new MatConfirmationService(dialog),
 };
