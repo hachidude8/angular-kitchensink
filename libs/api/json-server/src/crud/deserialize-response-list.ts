e2606176e5e738d214a2528c0c4c232aded9f498
hachidude8 <22824913+hachidude8@users.noreply.github.com>
2021-09-21 22:43:31 +0200

    Fixed deserializeResponseList implementation

--- libs/api/json-server/src/crud/deserialize-response-list.ts
+++ libs/api/json-server/src/crud/deserialize-response-list.ts
@@ -1,12 +1,10 @@
-import { DeserializeResponseListFn, PagedResponse } from '@aks/core/crud';
+import { DeserializeResponseListFn, PageDetails, PageImpl } from '@aks/core/crud';
 import { HttpResponse } from '@angular/common/http';
 import { PageValueRegex, PAGINATION_DETAILS_HEADER, PREV_LINK, TOTAL_ITEMS_SIZE_HEADER } from './constants';
 
 
-export const deserializeResponseList: DeserializeResponseListFn<
-  HttpResponse<Record<string, unknown>[]>,
-  Record<string, unknown>
-  > = (response) => {
+export const deserializeResponseList: DeserializeResponseListFn<HttpResponse<Record<string, unknown>[]>,
+  Record<string, unknown>> = (response) => {
   const { headers } = response;
   const totalSize = headers.get(TOTAL_ITEMS_SIZE_HEADER);
   const total = totalSize ? +totalSize : -1;
@@ -24,5 +22,12 @@ export const deserializeResponseList: DeserializeResponseListFn<
       previousOrNext.includes(PREV_LINK) ? page++ : page--;
     }
   }
-  return new PagedResponse(response.body || [], page, limit, total, total > -1);
-}
+  const source = response.body || [];
+  const details: PageDetails = {
+    pageIndex: page,
+    previousPageIndex: page - 1,
+    pageSize: limit,
+    length: total
+  };
+  return PageImpl.from(source, details);
+};
--- libs/api/json-server/src/crud/rest-resources-factory.ts
+++ libs/api/json-server/src/crud/rest-resources-factory.ts
@@ -4,8 +4,8 @@ import { RestResource, restResourceFactory, RestResourceStorage } from '@aks/cor
 export function restResourcesFactory(): RestResourceStorage {
   const baseUrl = 'http://localhost:3000';
   const resources = restResourceFactory({
-    users: new RestResource<number>(baseUrl, 'users'),
-    companies: new RestResource<number>(baseUrl, 'companies'),
+    users: RestResource.from(baseUrl, 'users', {}),
+    companies: RestResource.from(baseUrl, 'companies', {}),
   });
   console.debug(`Created a new http resources definition`, resources);
   return resources;
