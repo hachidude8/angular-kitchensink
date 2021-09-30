33d96ab256a03de05d50914df7127b590b12b4a0
hachidude8 <22824913+hachidude8@users.noreply.github.com>
2021-09-30 17:34:57 +0200

    Changed HttpResource accepted params definition Implemented URL creation methods

--- libs/core/crud/src/http-resource.ts
+++ libs/core/crud/src/http-resource.ts
@@ -1,3 +1,6 @@
+import { HttpParamType } from './query';
+
+
 /**
  * Data structure to store and handle a HTTP address.
  * This data structure allows the use of path parameters applied to {@var segment}
@@ -14,14 +17,30 @@ export class HttpResource {
   ) {
   }
 
-  url(params?: Record<string | number, string | number>): string {
-    const segment = this.serializeSegment(this.segment, params);
-    return `${this.baseUrl}/${segment}`;
+  url(params?: Record<string | number, HttpParamType>): string {
+    return this.createUrl(this.segment, params);
+  }
+
+  protected createUrl(segments: string | string[], params?: Record<string | number, HttpParamType>): string {
+    const source = Array.isArray(segments) ? segments : [segments];
+    const segment = this.combineSegments(source);
+    return this.serializeSegment(segment, params);
+  }
+
+  /**
+   * Combines the provided segments with the base URL
+   */
+  protected combineSegments(segments: string[] = []): string {
+    const base = `${this.baseUrl}`;
+    return segments.reduce((full, segment) => {
+      const copy = `${segment?.trim()}`;
+      return `${full}/${copy}`;
+    }, base);
   }
 
   protected serializeSegment(
     segment: string,
-    params?: Record<string | number, string | number>): string {
+    params?: Record<string | number, HttpParamType>): string {
     let copy = `${segment}`;
     if (params) {
       Object.entries(params).forEach(([key, value]) => {
